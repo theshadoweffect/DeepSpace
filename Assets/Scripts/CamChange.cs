4f8b107b12171b56975a22246c148e85e9f99ade
theshadoweffect <mcguiganriley@gmail.com>
2017-03-10 18:21:29 -0800

    First Commit

---
+++ Assets/Scripts/CamChange.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamChange : MonoBehaviour {
+
+	public Camera camShip;
+	public Camera camMap;
+	public GameObject playerShip;
+	public GameObject playerMapMove;
+	private bool mapStat;
+	private bool shipStat;
+	// Use this for initialization
+	void Start () { //sets systems standard
+
+		mapStat = false;
+		shipStat = true;
+		playerMapMove.SetActive (mapStat);
+		playerShip.SetActive (shipStat);
+		camShip.enabled = true;
+		camMap.enabled = false;
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.M)){//swaps cameras and disables none usable objects.
+			shipStat = !shipStat;//changes set active statements
+			mapStat = !mapStat;//changes set active statements
+			camShip.enabled = !camShip.enabled;
+			camMap.enabled = !camMap.enabled;
+			playerMapMove.SetActive (mapStat);
+			playerShip.SetActive (shipStat);
+
+		}
+	}
+}
---
+++ Assets/Scripts/ShipMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipMovement : MonoBehaviour {
+
+	//Acceleration Speeds
+	public float foreacl = 10;
+	public float strafacl = 10;
+	public float jumpacl = 10;
+
+	// Use this for initialization
+
+	// Update is called once per frame
+	void Update () {                    //(x, y, z)
+		//Accelerates object based on w,a,s,d
+		Vector3 movDirection = new Vector3 (strafacl * Input.GetAxis ("Horizontal"), jumpacl * Input.GetAxis ("Jump"), foreacl * Input.GetAxis ("Vertical"));// foreward back
+		this.GetComponent<Rigidbody> ().AddForce (movDirection);
+		//===============================
+		if (Input.GetKeyDown (KeyCode.Z)) {//stops all movement 
+			this.GetComponent<Rigidbody> ().velocity = new Vector3 (0,0,0);
+		}
+	}
+}
