e7d1a8284aad14de853a3712d45909d5b1ebf5c1
theshadoweffect <mcguiganriley@gmail.com>
2017-03-24 10:06:33 -0700

    Update

---
+++ Assets/Scripts/ConstructionScript.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstructionScript : MonoBehaviour {
+    public GameObject[] Buildables;
+    private float[] timestamp;
+    public float[] buildtime;
+    private int arrayLength;
+    private GameObject clone;
+    private int selector;
+	// Use this for initialization
+	void Start () {
+        arrayLength = Buildables.Length;
+        clone = Buildables[0];
+        selector = 0;
+        print(arrayLength);
+        timestamp = new float[arrayLength];
+        for (int ii = 0; ii < arrayLength; ii++) {
+            timestamp[ii] = Time.time + buildtime[ii];
+            print(timestamp);
+        }
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //modify Selector
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            selector = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            selector = 1;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            selector = 2;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            selector = 3;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            selector = 4;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            selector = 5;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha6))
+        {
+            selector = 6;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha7))
+        {
+            selector = 7;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha8))
+        {
+            selector = 8;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha9))
+        {
+            selector = 9;
+        }
+
+        //Selection check
+        if (selector >= arrayLength)
+        {
+            selector = arrayLength - 1;
+        }//Constructor access
+        if (timestamp[selector] <= Time.time) { 
+            if (Input.GetButton("Fire1"))
+             {
+                Construct();
+                timestamp[selector] = Time.time + buildtime[selector];
+             }
+         }
+    }
+    void Construct() {
+                                    //x y z
+        Vector3 offset = new Vector3(0, -20, 0);
+        clone = Instantiate(Buildables[selector], transform.position + offset, transform.rotation) as GameObject;
+
+    }
+
+
+}
---
+++ Assets/Scripts/WeaponSwitch.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSwitch : MonoBehaviour {
+    
+    public GameObject[] CannonArr;
+    public GameObject[] MissileArr;
+    public GameObject[] LaserArr;
+    public int numCannons = 4;
+    public int numMissiles = 4;
+    public int numLasers = 2;
+    
+ 
+    void Start() {//CANNONS ARE AUTOMATICALLY SELECTED FIRST
+        for (int ii = 0; ii < numCannons; ii++)
+        {
+            CannonArr[ii].SetActive(true);
+        }
+        for (int ii = 0; ii < numMissiles; ii++)
+        {
+            MissileArr[ii].SetActive(false);
+        }
+        for (int ii = 0; ii < numLasers; ii++)
+        {
+            LaserArr[ii].SetActive(false);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {//switches active weapons
+        //CANNON SELECT
+        if (Input.GetKeyDown(KeyCode.Alpha1)|| Input.GetKeyDown(KeyCode.Keypad1)) {
+            for (int ii = 0; ii < numCannons; ii++)
+            {
+                CannonArr[ii].SetActive(true);
+            }
+            for (int ii = 0; ii < numMissiles; ii++)
+            {
+                MissileArr[ii].SetActive(false);
+            }
+            for (int ii = 0; ii < numLasers; ii++)
+            {
+                LaserArr[ii].SetActive(false);
+            }
+        }//MISSILE SELECT
+        if (Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKeyDown(KeyCode.Keypad2))
+        {
+            for (int ii = 0; ii < numCannons; ii++)
+            {
+                CannonArr[ii].SetActive(false);
+            }
+            for (int ii = 0; ii < numMissiles; ii++)
+            {
+                MissileArr[ii].SetActive(true);
+            }
+            for (int ii = 0; ii < numLasers; ii++)
+            {
+                LaserArr[ii].SetActive(false);
+            }
+        }//LASER SELECT
+        if (Input.GetKeyDown(KeyCode.Alpha3) || Input.GetKeyDown(KeyCode.Keypad3))
+        {
+            for (int ii = 0; ii < numCannons; ii++)
+            {
+                CannonArr[ii].SetActive(false);
+            }
+            for (int ii = 0; ii < numMissiles; ii++)
+            {
+                MissileArr[ii].SetActive(false);
+            }
+            for (int ii = 0; ii < numLasers; ii++)
+            {
+                LaserArr[ii].SetActive(true);
+            }
+        }
+    }
+}
