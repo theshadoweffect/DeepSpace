6f24a597097c1dd782061fc91252ee46c9b709f8
cheweththecud <theelderking@yahoo.com>
2017-04-13 15:07:07 -0700

    hud
    put in a simple hud and some effects for the player engine.

---
+++ Assets/healthBar.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Image=UnityEngine.UI.Image;
+
+public class healthBar : MonoBehaviour {
+    Image HealthBar;
+    float tmpHealth=1;
+	// Use this for initialization
+	void Start () {
+        HealthBar=GameObject.Find("HealthBar").transform.FindChild("Foreground").GetComponent <Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        HealthBar.fillAmount = tmpHealth;
+	}
+}
