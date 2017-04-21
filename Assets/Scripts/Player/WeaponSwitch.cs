814dd3423e94860dc6a5f3478cddd47192dfdf30
King <cody.king@van-l-mm111-81.vancouver.wsu.edu>
2017-04-21 16:18:48 -0700

    hud and sound

--- Assets/Scripts/Player/WeaponSwitch.cs
+++ Assets/Scripts/Player/WeaponSwitch.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class WeaponSwitch : MonoBehaviour {
     
     public GameObject[] CannonArr;
@@ -10,12 +10,16 @@ public class WeaponSwitch : MonoBehaviour {
     int numCannons;
     int numMissiles;
     int numLasers;
-    
- 
+	public Image CannonRect;
+	public Image MissileRect;
+	public Image LaserRect;
     void Start() {//CANNONS ARE AUTOMATICALLY SELECTED FIRST
         numCannons = CannonArr.Length;
         numMissiles = MissileArr.Length;
         numLasers = LaserArr.Length;
+		CannonRect.enabled = true;
+		MissileRect.enabled = false;
+		LaserRect.enabled = false;
         for (int ii = 0; ii < numCannons; ii++)
         {
             CannonArr[ii].SetActive(true);
@@ -33,7 +37,11 @@ public class WeaponSwitch : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {//switches active weapons
         //CANNON SELECT
-        if (Input.GetKeyDown(KeyCode.Alpha1)|| Input.GetKeyDown(KeyCode.Keypad1)) {
+        if (Input.GetKeyDown(KeyCode.Alpha1)|| Input.GetKeyDown(KeyCode.Keypad1)) 
+		{
+			CannonRect.enabled = true;
+			MissileRect.enabled = false;
+			LaserRect.enabled = false;
             for (int ii = 0; ii < numCannons; ii++)
             {
                 CannonArr[ii].SetActive(true);
@@ -49,6 +57,9 @@ public class WeaponSwitch : MonoBehaviour {
         }//MISSILE SELECT
         if (Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKeyDown(KeyCode.Keypad2))
         {
+			CannonRect.enabled = false;
+			MissileRect.enabled = true;
+			LaserRect.enabled = false;
             for (int ii = 0; ii < numCannons; ii++)
             {
                 CannonArr[ii].SetActive(false);
@@ -64,6 +75,9 @@ public class WeaponSwitch : MonoBehaviour {
         }//LASER SELECT
         if (Input.GetKeyDown(KeyCode.Alpha3) || Input.GetKeyDown(KeyCode.Keypad3))
         {
+			CannonRect.enabled = false;
+			MissileRect.enabled = false;
+			LaserRect.enabled = true;
             for (int ii = 0; ii < numCannons; ii++)
             {
                 CannonArr[ii].SetActive(false);
